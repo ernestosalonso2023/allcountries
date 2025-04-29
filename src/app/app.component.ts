4151fbe9da6b52ce08188741e52cece44f7366af
Ernesto <ernestosalonso2023@gmail.com>
2025-04-29 20:21:36 +0200

    modified:   angular.json 	modified:   docs/allcountries/browser/404.html 	modified:   docs/allcountries/browser/index.html 	renamed:    docs/allcountries/browser/main-LU6TDP6J.js -> docs/allcountries/browser/main-FMBFD4IR.js 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/detailscountry/detailscountry.component.html 	modified:   src/index.html 	new file:   ~$iframe map.xlsx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardcountryComponent } from "./cardcountry/cardcountry.component";
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpEventType, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpRequest, HttpStatusCode } from '@angular/common/http';
 import { Country } from './country';
 import { DetailscountryComponent } from './detailscountry/detailscountry.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardcountryComponent, CommonModule, DetailscountryComponent],
+  imports: [RouterOutlet, CardcountryComponent, CommonModule, FormsModule, DetailscountryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  //nombre="Ernesto";
+ // entrada="mmm";
   title = 'allcountries';
   viewlist = true;
   loaded=false;
@@ -40,7 +43,7 @@ export class AppComponent {
   }
   getAllCountry() {
     const req = new HttpRequest("GET", "https://restcountries.com/v3.1/all/?fields=name,capital,population,area,flags",
-      { reportProgress: true, responseType: "json" }
+      { reportProgress: true, responseType: "json", }
     );
     this.http.request<Country[]>(req).subscribe(event => {
       if (event.type === HttpEventType.DownloadProgress) {
@@ -55,11 +58,15 @@ export class AppComponent {
           this.countries = event.body
           this.progress=100;
         }
-    });
+   });
     /*this.http.get<Country[]>('https://restcountries.com/v3.1/all/?fields=name,capital,population,area,flags')
       .subscribe(data => {
         this.countries = data;
         //console.log(this.countries);
       });*/
+  }
+  Buscando(){
+    //alert(this.entrada);
+    //this.nombre=this.entrada;
   }
 }
